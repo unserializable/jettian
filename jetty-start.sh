e767dabd4aa1740b3110acb89e956ebc8e37055d
Taimo Peelo <unserializable@timedrops.us>
2015-11-18 09:28:38 +0200

    Allow start/stop script executions from anywhere, take jetty.base to be the script location.

--- jetty-start.sh
+++ jetty-start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -jar ../start.jar STOP.KEY=stop_jetty_plz STOP.PORT=13450
+SCRIPT_DIR=$(dirname $0)
+java -Djetty.base="$SCRIPT_DIR" -jar "$SCRIPT_DIR/../start.jar" STOP.KEY=stop_jetty_plz STOP.PORT=13450
